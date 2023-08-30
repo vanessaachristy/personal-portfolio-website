788c24647fcf750ccc6b694c124b683af84267d6
Vanessa Christy <vanessachristy@Vanessas-MacBook-Air.local>
2023-08-30 23:38:40 +0800

    add new job

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,23 +1,41 @@
-import { React, useEffect } from "react";
-import Aos from "aos";
-import "./work.css";
-import singtel from "./singtel.png";
+import { React, useEffect } from 'react'
+import Aos from 'aos'
+import './work.css'
+import { workExperience } from './config'
 
 export default function Work() {
-  useEffect(() => {
-    Aos.init();
-  }, []);
-  return (
-    <div className="work" id="work">
-      <div className="work-title" data-aos="fade-up">
-        work experience.
-      </div>
-      <div className="work-container">
-        <div className="work-box" data-aos="zoom-in">
-          <div className="work-logo">
-            <img src={singtel} alt="" />
-          </div>
-          <div className="work-description">
+    useEffect(() => {
+        Aos.init()
+    }, [])
+    return (
+        <div className="work" id="work">
+            <div className="work-title" data-aos="fade-up">
+                work experience.
+            </div>
+            <div className="work-container">
+                {workExperience.map((work) => {
+                    return (
+                        <div className="work-box" data-aos="zoom-in">
+                            <div className="work-logo">
+                                <img src={work.logo} alt="" />
+                            </div>
+                            <div className="work-description">
+                                <h1>{work.name}</h1>
+                                <p>
+                                    <i>{work.period}</i>
+                                </p>
+                                <h3>{work.department}</h3>
+                                <ul>
+                                    {work.jobscope.map((job) => {
+                                        return <li>{job}</li>
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    )
+                })}
+
+                {/* <div className="work-description">
             <h1>Software Developer Intern, Singtel</h1>
             <p>
               <i>May 2022 - Aug 2022</i>
@@ -30,9 +48,10 @@ export default function Work() {
             </p>
             <p>• Build the REST APIs for the services using Spring Boot</p>
             <p>• Create unit testing for the REST services using JUnit</p>
-          </div>
+
+            
+          </div> */}
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
---
+++ src/components/work/config.js
@@ -0,0 +1,26 @@
+export const workExperience = [
+     {
+        name: "Frontend Engineer Intern, TikTok",
+        period: "Jan 2023 - Present",
+        department: "Global E-Commerce (Commercial Platform)",
+        jobscope: [
+            "Designed and developed efficient, reusable micro-frontend components for the TikTok Shop Seller Centre PC & App Data Compass platform using React, Typescript, Rematch",
+            "Collaborated with Product Management, Product Design, and Software Engineering teams in the creation of high-performance, innovative e-commerce solutions",
+            "Assisted internal teams in seamlessly incorporating internationalization and localization SDK toolsets, enhancing engineering efficiency across the board",
+            "Contributed to enhancing project and codebase quality, optimizing performance, addressed security matters, fine-tuned checking processes, and managed the migration of libraries for improved outcomes"
+        ],
+        logo: 'assets/tiktok.png'
+    },
+    {
+        name: "Software Developer Intern, Singtel",
+        period: "May 2022 - Aug 2022",
+        department: "Group Enterprise 5G/IOT Product House",
+        jobscope: [
+            "Developed the UI for a Credential Management Web Application using Vue.js",
+            "Build the REST APIs for the services using Spring Boot",
+            "Create unit testing for the REST services using JUnit"
+        ],
+        logo: 'assets/singtel.png'
+    },
+    
+]
